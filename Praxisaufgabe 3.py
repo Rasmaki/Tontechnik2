8310205abb8c58e3f4c35b59b6c41ef5738a4ad4
Robert Schleßmann <robert.arthur@web.de>
2025-11-15 15:44:27 +0100

    Added Praxisaufgabe 3.py Added wav files for processing Added calculations for energy and correlation

---
+++ Praxisaufgabe 3.py
@@ -0,0 +1,38 @@
+"""
+Title:
+Author:
+Kurs: Dies ist ein Test
+"""
+
+import numpy as np
+import librosa
+import sounddevice as sd
+
+tracks = ['./multitrack/01_SaxophoneCloseMic1.wav',
+          './multitrack/02_SaxophoneCloseMic2.wav',
+          './multitrack/03_SaxophoneFarMic1.wav',
+          './multitrack/04_AcousticGtrCloseMics.wav',
+          './multitrack/05_AcousticGtrFarMics.wav',
+          './multitrack/06_AcousticGtrDI.wav']
+energy = []
+
+
+def calc_e(sig, sample_rate):
+    return sum(sig**2)*(1/sample_rate)
+
+
+def calc_corr(sig1, sig2, sample_rate):
+    return (sum(sig1*sig2))/np.sqrt((sum(sig1**2))*(sum(sig2**2)))
+
+
+for i in range(len(tracks)):
+    x = librosa.load(tracks[i])[0]
+    fs = librosa.load(tracks[i])[1]
+    energy.append(calc_e(x, fs))
+
+energy_sum = sum(energy)
+corr = calc_corr(librosa.load(tracks[0])[0], librosa.load(tracks[1])[0], librosa.load(tracks[0])[1])
+print("Einzelenergien:", energy)
+print("Gesamtenergie:", energy_sum)
+print("Korrelationsgrad", corr)
+
